8edca0649c0056fc4adf6b28167daef8687a8da3
timjcai <tim.j.cai@gmail.com>
2024-01-18 12:16:30 +1100

    chores: update styling - making it easier to read

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,10 @@ import RGBColorpicker from "./components/Colorpicker/RGBColorpicker";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-screen flex-col items-center p-24">
             <h1 className="text-2xl font-bold">RGB Colorpicker</h1>
-            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+            <p>@timjcai</p>
+            <div className="z-10 max-w-6xl w-full items-center justify-between font-mono text-sm lg:flex">
                 <RGBColorpicker />
             </div>
         </main>
