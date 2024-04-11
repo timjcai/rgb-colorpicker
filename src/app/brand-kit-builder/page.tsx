a77c70ed9fef47f9de76e861461f5d274906b07b
Tim Cai <tim.j.cai@gmail.com>
2024-04-11 11:52:27 +1000

    feat: completed Landing Page design - waiting for header image

--- src/app/brand-kit-builder/page.tsx
+++ src/app/brand-kit-builder/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const page = () => {
-    return <div></div>;
+    return (
+        <div className="flex items-center justify-center">
+            <div className="w-[80vw] min-h-screen">
+                <Navbar />
+            </div>
+        </div>
+    );
 };
 
 export default page;
