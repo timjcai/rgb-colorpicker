b6c9aaf36f7b284f9d5221fd0a9c8e9fedef07e3
Tim Cai <tim.j.cai@gmail.com>
2024-04-23 09:55:44 +1000

    bugfix: updating responsiveness - colorpicker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,11 @@ import RGBColorpicker from "./components/Colorpicker/RGBColorpicker";
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center p-24">
-            <h1 className="text-2xl font-bold">RGB Colorpicker</h1>
-            <p>@timjcai</p>
-            <div className="z-10 max-w-6xl w-full items-center justify-between font-mono text-sm lg:flex">
+            <div className="mb-8">
+                <h1 className="text-2xl font-bold">RGB Colorpicker</h1>
+                <p>@timjcai</p>
+            </div>
+            <div className="max-w-6xl w-full items-center justify-between font-mono text-sm lg:flex mt-8">
                 <RGBColorpicker />
             </div>
         </main>
